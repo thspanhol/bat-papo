6533b9524d6027a8dde1c0bccc6e05998a30a1c3
Thales Spanhol <thales_spanhol@hotmail.com>
2023-07-06 19:03:01 -0300

    clear app, services and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import React, {useEffect, useState} from "react";
-import { useAuthState } from 'react-firebase-hooks/auth';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "./services/firebase";
 import Login from "./components/Login";
 import Loading from "./components/Loading";
-import Sidebar from './components/Sidebar';
-import * as C from './styles/app'
+import Sidebar from "./components/Sidebar";
+import * as C from "./styles/app";
 import Chat from "./components/Chat";
 
 const App = () => {
-
   const [user, loading] = useAuthState(auth);
   const [userChat, setUserChat] = useState(null);
 
   useEffect(() => {
     if (user) {
-      db.collection('users').doc(user.uid).set({
+      db.collection("users").doc(user.uid).set({
         email: user.email,
         photoURL: user.photoURL,
       });
     }
   }, [user]);
 
-  if (loading) return <Loading />
+  if (loading) return <Loading />;
 
-  if (!user) return <Login />
+  if (!user) return <Login />;
 
   return (
     <C.Container>
-     <Sidebar setUserChat={setUserChat} userChat={userChat} />
-     <Chat userChat={userChat} />
+      <Sidebar setUserChat={setUserChat} userChat={userChat} />
+      <Chat userChat={userChat} />
     </C.Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,20 +1,20 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCoP7I3UyUUMzUQX3MNgxQ1FfOG6J3hFg4",
-    authDomain: "bat-papo-a2d50.firebaseapp.com",
-    projectId: "bat-papo-a2d50",
-    storageBucket: "bat-papo-a2d50.appspot.com",
-    messagingSenderId: "464804412923",
-    appId: "1:464804412923:web:5d79856186bf3b13135f5e"
-  };
+  apiKey: "AIzaSyCoP7I3UyUUMzUQX3MNgxQ1FfOG6J3hFg4",
+  authDomain: "bat-papo-a2d50.firebaseapp.com",
+  projectId: "bat-papo-a2d50",
+  storageBucket: "bat-papo-a2d50.appspot.com",
+  messagingSenderId: "464804412923",
+  appId: "1:464804412923:web:5d79856186bf3b13135f5e",
+};
 
-  const app = firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
-  const db = app.firestore();
-  const auth = app.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-  export {db, auth, provider};
+export { db, auth, provider };
