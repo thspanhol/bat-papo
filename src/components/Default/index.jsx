fb0660d6a30e5dfe9b83944db7393cc0a58cc0b1
Thales Spanhol <thales_spanhol@hotmail.com>
2023-07-09 20:18:16 -0300

    Refactor: refatoração dos componentes restantes

--- src/components/Default/index.jsx
+++ src/components/Default/index.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import * as C from './styles'
-import { MdMessage } from 'react-icons/md'
+import React from "react";
+import * as C from "./styles";
+import { MdMessage } from "react-icons/md";
 
 const Default = () => {
   return (
     <C.Container>
-        <MdMessage />
-        <C.Title>Chat App</C.Title>
-        <C.Info>
-            Agora você pode enviar e receber mensagens sem precisar manter seu celular conectado à internet.
-        </C.Info>
+      <MdMessage />
+      <C.Title>Chat App</C.Title>
+      <C.Info>
+        Agora você pode enviar e receber mensagens sem precisar manter seu
+        celular conectado à internet.
+      </C.Info>
     </C.Container>
-  )
-}
+  );
+};
 
-export default Default
+export default Default;
